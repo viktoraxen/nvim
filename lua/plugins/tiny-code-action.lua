77adfae2caa9fef09fb6b3014287299817822f2a
evikaxe <evikaxe>
2025-08-12 14:16:04 +0200

    tinycodeaction relative position

--- lua/plugins/tiny-code-action.lua
+++ lua/plugins/tiny-code-action.lua
@@ -1,10 +1,24 @@
-return
-{
+return {
     "rachartier/tiny-code-action.nvim",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
     },
     event = "LspAttach",
+    config = function()
+        local codeaction = require("tiny-code-action")
+        codeaction.setup({
+            backend = "delta",
+            picker = {
+                "buffer",
+                position = "cursor",
+                auto_preview = "true"
+            }
+        })
+
+        local map = require("utils.keymap")
+
+        map.n("gra", codeaction.code_action, "Code actions")
+    end,
     opts = {
         backend = "vim",
         picker = "buffer",
