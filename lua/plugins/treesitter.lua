6da5ca8d3ea169ada07affedb24e6afd40c74e0c
Viktor Axén <vikax460@student.liu.se>
2025-08-17 15:33:28 +0200

    tree-sitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,21 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
-    event = { "VimEnter" },
-    opts = {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'javascript' },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { 'ruby', 'javascript' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-        -- Prefer git instead of curl in order to improve connectivity in some environments
-        require('nvim-treesitter.install').prefer_git = true
-        ---@diagnostic disable-next-line: missing-fields
-        require('nvim-treesitter.configs').setup(opts)
-    end,
+    config = function()
+        require('nvim-treesitter').install({
+            'bash',
+            'c',
+            'diff',
+            'html',
+            'javascript',
+            'lua',
+            'luadoc',
+            'markdown',
+            'python',
+            'vim',
+            'vimdoc',
+        })
+    end
 }
