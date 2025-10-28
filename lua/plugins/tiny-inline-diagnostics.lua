edcadd7e97c5e796acddd521305ef7b4cfa13013
Viktor Axén <vikax460@student.liu.se>
2025-10-28 10:42:41 +0100

    tiny-inline-diagnostic style changes

--- lua/plugins/tiny-inline-diagnostics.lua
+++ lua/plugins/tiny-inline-diagnostics.lua
@@ -4,7 +4,8 @@ return {
         event = "VeryLazy",
         priority = 1000,
         opts = {
-            preset = "simple",
+            preset = "classic",
+            transparent_bg = true,
             options = {
                 add_messages = { display_count = true },
                 -- show_source = { enabled = true, if_many = true },
@@ -12,6 +13,8 @@ return {
                 show_all_diags_on_cursorline = true,
                 enable_on_insert = true,
                 overflow = { padding = -8 },
+                use_icons_from_diagnostic = true,
+                override_open_float = true,
             },
         },
     },
