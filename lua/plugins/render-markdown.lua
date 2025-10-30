e5f9dddb4096fb85a32d815a722ed64d124caef1
Viktor Axén <vikax460@student.liu.se>
2025-10-30 11:36:44 +0100

    added render-markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,41 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-tree/nvim-web-devicons'
+    },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        anti_conceal = {
+            enabled = false,
+            ignore = {
+                table_border = true,
+            }
+        },
+        heading = {
+            sign = false,
+            position = 'inline',
+        },
+        code = {
+            sign = false,
+            border = 'thick',
+            inline_pad = 1,
+            left_pad = 2,
+        },
+        overrides = {
+            buftype = {
+                -- LSP Hover window
+                nofile = {
+                    code = {
+                        left_pad = 0,
+                        right_pad = 0,
+                        style = 'language',
+                        border = 'hide',
+                        language = false,
+                    },
+                }
+            }
+        }
+    },
+}
