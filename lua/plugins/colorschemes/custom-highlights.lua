1e62198055ac7cbad0e746bc41024598ebc6d203
Viktor Axén <vikax460@student.liu.se>
2025-11-03 18:58:18 +0100

    diagnostic hls remove italic

--- lua/plugins/colorschemes/custom-highlights.lua
+++ lua/plugins/colorschemes/custom-highlights.lua
@@ -15,6 +15,13 @@ return {
                     LightFloatTitle = { fg = "surface0", bg = "sky" },
                     DarkFloat = { bg = "mantle" },
                     DarkFloatTitle = { fg = "mantle", bg = "sky" },
+
+                    DiagnosticOk = { italic = false, cterm = { italic = false } },
+                    DiagnosticDebug = { italic = false, cterm = { italic = false } },
+                    DiagnosticHint = { italic = false, cterm = { italic = false } },
+                    DiagnosticInfo = { italic = false, cterm = { italic = false } },
+                    DiagnosticWarn = { italic = false, cterm = { italic = false } },
+                    DiagnosticError = { italic = false, cterm = { italic = false } },
                 },
             },
             links = {
