a555013233177bfed7c603fe31befa4640894e9f
Viktor Axén <vikax460@student.liu.se>
2025-11-15 15:09:24 +0100

    treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,62 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
     lazy = false,
-    opts = {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'javascript' },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { 'ruby', 'javascript' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-        require('nvim-treesitter.install').prefer_git = true
-        require('nvim-treesitter.configs').setup(opts)
+    init = function()
+        local ensure_installed = {
+            'c',
+            'lua',
+            'markdown',
+            'markdown_inline',
+            'query',
+            'vim',
+            'vimdoc',
+            -- NOTE: the above are natively installed since neovim 0.12
+            'bash',
+            'diff',
+            'dockerfile',
+            'gitignore',
+            'git_config',
+            'luadoc',
+            'regex',
+            'toml',
+            'yaml',
+            'csv',
+            'java',
+            'python',
+            'html',
+            'css',
+            'javascript',
+            'typescript',
+            'json',
+        }
+
+        local isnt_installed = function(lang)
+            return #vim.api.nvim_get_runtime_file('parser/' .. lang .. '.*', false) == 0
+        end
+
+        local to_install = vim.tbl_filter(isnt_installed, ensure_installed)
+        if #to_install > 0 then require('nvim-treesitter').install(to_install) end
+
+        -- Ensure tree-sitter enabled after opening a file for target language
+        local filetypes = {}
+
+        for _, lang in ipairs(ensure_installed) do
+            for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+                table.insert(filetypes, ft)
+            end
+        end
+
+        local ts_start = function(ev) vim.treesitter.start(ev.buf) end
+
+        -- WARN: Do not use "*" here - snacks.nvim is buggy and vim.notify triggers FileType events internally causing infinite callback loops
+        vim.api.nvim_create_autocmd('FileType', {
+            desc = 'Start treesitter',
+            group = vim.api.nvim_create_augroup('start_treesitter', { clear = true }),
+            pattern = filetypes,
+            callback = ts_start,
+        })
     end,
+    config = function() require('nvim-treesitter').setup() end,
 }
