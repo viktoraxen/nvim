240cc4d5f0a5d8738e2cc9c7dae0e928fe6a631f
Viktor Axén <vikax460@student.liu.se>
2025-11-15 15:12:38 +0100

    removed autocommand scrolloff at eof

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -116,24 +116,24 @@ vim.api.nvim_create_autocmd('BufRead', {
     end
 })
 
-vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "WinScrolled" }, {
-    desc = "Fix scrolloff when you are at the EOF",
-    group = vim.api.nvim_create_augroup("ScrollEOF", { clear = true }),
-    callback = function()
-        if vim.api.nvim_win_get_config(0).relative ~= "" then
-            return -- Ignore floating windows
-        end
-
-        local win_height = vim.fn.winheight(0)
-        local scrolloff = math.min(vim.o.scrolloff, math.floor(win_height / 2))
-        local visual_distance_to_eof = win_height - vim.fn.winline()
-
-        if visual_distance_to_eof < scrolloff then
-            local win_view = vim.fn.winsaveview()
-            vim.fn.winrestview({ topline = win_view.topline + scrolloff - visual_distance_to_eof })
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "WinScrolled" }, {
+--     desc = "Fix scrolloff when you are at the EOF",
+--     group = vim.api.nvim_create_augroup("ScrollEOF", { clear = true }),
+--     callback = function()
+--         if vim.api.nvim_win_get_config(0).relative ~= "" then
+--             return -- Ignore floating windows
+--         end
+--
+--         local win_height = vim.fn.winheight(0)
+--         local scrolloff = math.min(vim.opt.scrolloff, math.floor(win_height / 2))
+--         local visual_distance_to_eof = win_height - vim.fn.winline()
+--
+--         if visual_distance_to_eof < scrolloff then
+--             local win_view = vim.fn.winsaveview()
+--             vim.fn.winrestview({ topline = win_view.topline + scrolloff - visual_distance_to_eof })
+--         end
+--     end,
+-- })
 
 vim.api.nvim_create_autocmd({ "ColorScheme", "UIEnter" }, {
     desc = "Corrects terminal background color according to colorscheme",
