dbb5f52f20570ccd8ca28c8367ba205546c00214
Viktor Axén <vikax460@student.liu.se>
2025-11-24 23:05:17 +0100

    vscode diff

---
+++ lua/plugins/vscode-diff.lua
@@ -0,0 +1,6 @@
+return {
+  'esmuellert/vscode-diff.nvim',
+  cmd = { 'CodeDiff' },
+  keys = { { '<leader>gd', ':CodeDiff file HEAD<cr>', desc = 'Diff current file' } },
+  dependencies = { 'MunifTanjim/nui.nvim' },
+}
