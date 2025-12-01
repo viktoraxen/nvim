d648176fabf99777c21b52fb8aeef0bf8761e70d
Viktor Axén <vikax460@student.liu.se>
2025-12-01 10:33:55 +0100

    formatted

--- lua/utils/keymap.lua
+++ lua/utils/keymap.lua
@@ -5,35 +5,35 @@ local map = function(mode, key, action, desc)
 end
 
 M.o = function(key, action, desc)
-  map('o', key, action, desc)
+  map("o", key, action, desc)
 end
 
 M.n = function(key, action, desc)
-  map('n', key, action, desc)
+  map("n", key, action, desc)
 end
 
 M.v = function(key, action, desc)
-  map('v', key, action, desc)
+  map("v", key, action, desc)
 end
 
 M.t = function(key, action, desc)
-  map('t', key, action, desc)
+  map("t", key, action, desc)
 end
 
 M.i = function(key, action, desc)
-  map('i', key, action, desc)
+  map("i", key, action, desc)
 end
 
 M.ln = function(key, action, desc)
-  M.n('<leader>' .. key, action, desc)
+  M.n("<leader>" .. key, action, desc)
 end
 
 M.l_group = function(key, desc)
-  require('which-key').add { mode = { 'n', 'v' }, { '<leader>' .. key, group = desc } }
+  require("which-key").add({ mode = { "n", "v" }, { "<leader>" .. key, group = desc } })
 end
 
 M.group = function(key, desc)
-  require('which-key').add { mode = { 'n', 'v' }, { key, group = desc } }
+  require("which-key").add({ mode = { "n", "v" }, { key, group = desc } })
 end
 
 return M
