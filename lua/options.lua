a3dc233897719038a11299592390fabac6cc9342
Viktor Axén <viktor.axen@gmail.com>
2026-01-04 16:47:45 +0100

    ssh clipboard sync

--- lua/options.lua
+++ lua/options.lua
@@ -18,6 +18,20 @@ vim.opt.showmode = false
 -- Sync clipboard between OS and Neovim.
 vim.opt.clipboard = "unnamedplus"
 
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
+
 -- Enable break indent
 vim.opt.breakindent = true
 
