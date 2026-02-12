3353359e7736d965d09bd5203c56e1b055bf39b1
Viktor Axén <viktor.axen@gmail.com>
2026-02-13 00:06:59 +0100

    lazy border color fix

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -15,6 +15,18 @@ require("lazy").setup({
   },
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lazy",
+  callback = function()
+    local win = vim.api.nvim_get_current_win()
+    vim.schedule(function()
+      if vim.api.nvim_win_is_valid(win) then
+        vim.wo[win].winhighlight = "Normal:LazyNormal,FloatBorder:LazyNormal"
+      end
+    end)
+  end,
+})
+
 local map = require("utils.keymap")
 
 map.n("<leader>L", "<cmd>Lazy<cr>", "Lazy")
