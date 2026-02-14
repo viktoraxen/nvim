db5cd75225df906c56331977b4630779bf994dec
Viktor Axén <viktor.axen@gmail.com>
2026-02-15 00:48:16 +0100

    treesitter setup configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -28,6 +28,6 @@ return {
     -- Prefer git instead of curl in order to improve connectivity in some environments
     require("nvim-treesitter.install").prefer_git = true
     ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter").setup(opts)
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
