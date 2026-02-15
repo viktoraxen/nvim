26801235444c9e2e0fd8a2a1a28c92b67865c818
Viktor Axén <viktor.axen@gmail.com>
2026-02-15 16:44:01 +0100

    persistance close unrestorable buffers before saving session

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -2,4 +2,21 @@ return {
   "folke/persistence.nvim",
   event = "BufReadPre",
   opts = {},
+  config = function(_, opts)
+    require("persistence").setup(opts)
+
+    -- Close windows that don't restore well before saving the session
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "PersistenceSavePre",
+      callback = function()
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local bt = vim.bo[buf].buftype
+          if bt == "terminal" or bt == "help" then
+            pcall(vim.api.nvim_win_close, win, true)
+          end
+        end
+      end,
+    })
+  end,
 }
