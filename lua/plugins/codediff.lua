bc1e6f07cda10f0ec06abd9432d03221f3c5a2b5
Viktor Axén <viktor.axen@gmail.com>
2026-02-15 16:38:59 +0100

    codediff tab reenter removed keybindigns bugfix

--- lua/plugins/codediff.lua
+++ lua/plugins/codediff.lua
@@ -26,4 +26,30 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("codediff").setup(opts)
+
+    -- Workaround: TabLeave clears explorer keymaps but TabEnter only restores
+    -- view keymaps. Re-apply explorer keymaps when returning to a codediff tab.
+    vim.api.nvim_create_autocmd("TabEnter", {
+      callback = function()
+        vim.schedule(function()
+          local ok, lifecycle = pcall(require, "codediff.ui.lifecycle")
+          if not ok then
+            return
+          end
+          local tabpage = vim.api.nvim_get_current_tabpage()
+          local explorer = lifecycle.get_explorer(tabpage)
+          if explorer then
+            require("codediff.ui.explorer.keymaps").setup(explorer)
+            local ob, mb = lifecycle.get_buffers(tabpage)
+            if ob and mb then
+              local is_explorer = lifecycle.get_mode(tabpage) == "explorer"
+              require("codediff.ui.view.keymaps").setup_all_keymaps(tabpage, ob, mb, is_explorer)
+            end
+          end
+        end)
+      end,
+    })
+  end,
 }
