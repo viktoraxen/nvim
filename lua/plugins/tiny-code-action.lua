1151e8637d89019261d3d223d5140d42bb90730e
Viktor Axén <viktor.axen@gmail.com>
2026-02-16 21:54:19 +0100

    tiny-code-action config cleanup

--- lua/plugins/tiny-code-action.lua
+++ lua/plugins/tiny-code-action.lua
@@ -4,35 +4,31 @@ return {
     { "nvim-lua/plenary.nvim" },
   },
   event = "LspAttach",
-  config = function()
-    local codeaction = require("tiny-code-action")
-    codeaction.setup({
-      backend = "delta",
-      picker = {
-        "buffer",
-        position = "cursor",
-        auto_preview = "true",
-      },
-    })
-
-    local map = require("utils.keymap")
-
-    map.n("gra", codeaction.code_action, "Code actions")
-  end,
   opts = {
-    backend = "vim",
-    picker = "buffer",
+    backend = "delta",
+    picker = {
+      "buffer",
+      position = "cursor",
+      auto_preview = "true",
+    },
     signs = {
-      quickfix = { "", { link = "DiagnosticWarning" } },
-      others = { "", { link = "DiagnosticWarning" } },
+      quickfix = { "", { link = "DiagnosticWarning" } },
+      others = { "", { link = "DiagnosticWarning" } },
       refactor = { "󰩫", { link = "DiagnosticInfo" } },
       ["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
-      ["refactor.extract"] = { "", { link = "DiagnosticError" } },
-      ["source.organizeImports"] = { "", { link = "DiagnosticWarning" } },
+      ["refactor.extract"] = { "", { link = "DiagnosticError" } },
+      ["source.organizeImports"] = { "", { link = "DiagnosticWarning" } },
       ["source.fixAll"] = { "󰃢", { link = "DiagnosticError" } },
-      ["source"] = { "", { link = "DiagnosticError" } },
+      ["source"] = { "", { link = "DiagnosticError" } },
       ["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
-      ["codeAction"] = { "", { link = "DiagnosticWarning" } },
+      ["codeAction"] = { "", { link = "DiagnosticWarning" } },
     },
   },
+  config = function(_, opts)
+    local codeaction = require("tiny-code-action")
+    codeaction.setup(opts)
+
+    local map = require("utils.keymap")
+    map.n("gra", codeaction.code_action, "Code actions")
+  end,
 }
