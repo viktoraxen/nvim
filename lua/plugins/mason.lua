c76c6134a15fac875c22f1abac1f4bf80fd24b95
Viktor Axén <viktor.axen@gmail.com>
2026-02-16 21:54:09 +0100

    mason removed ensure installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,23 +3,6 @@ return {
   keys = { { "<leader>M", "<cmd>Mason<cr>", desc = "Mason" } },
   cmd = { "Mason" },
   opts = {
-    ensure_installed = {
-      "bash-language-server",
-      "beautysh",
-      "clangd",
-      "cmake-language-server",
-      "cmakelang",
-      "codelldb",
-      "eslint-lsp",
-      "lua-language-server",
-      "markdownlint",
-      "prettier",
-      "pyright",
-      "python-lsp-server",
-      "ruff",
-      "stylua",
-      "tree-sitter-cli",
-    },
     ui = {
       check_outdated_packages_on_open = true,
       border = require("config").border,
