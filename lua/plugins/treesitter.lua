7b543c1293f99cdcd76f7b291d148422ba6b96c1
Viktor Axén <viktor.axen@gmail.com>
2026-02-17 18:19:43 +0100

    treesitter init fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,31 +2,43 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile", "SessionLoadPost" },
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "cpp",
-      "diff",
-      "html",
-      "javascript",
-      "lua",
-      "luadoc",
-      "markdown",
-      "python",
-      "vim",
-      "vimdoc",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    -- Prefer git instead of curl in order to improve connectivity in some environments
-    require("nvim-treesitter.install").prefer_git = true
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup(opts)
+  config = function()
+    local has_configs, configs = pcall(require, "nvim-treesitter.configs")
+
+    if has_configs then
+      -- Old API (master branch): modules managed by the plugin
+      require("nvim-treesitter.install").prefer_git = true
+      ---@diagnostic disable-next-line: missing-fields
+      configs.setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "cpp",
+          "diff",
+          "html",
+          "javascript",
+          "lua",
+          "luadoc",
+          "markdown",
+          "python",
+          "vim",
+          "vimdoc",
+        },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    else
+      -- New API (main branch): use Neovim built-in treesitter
+      require("nvim-treesitter").setup({})
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+          if vim.treesitter.query.get(vim.treesitter.language.get_lang(vim.bo.filetype) or "", "indents") then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end
   end,
 }
