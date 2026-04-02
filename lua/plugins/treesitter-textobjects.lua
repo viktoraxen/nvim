71c312a6fd35621cbc64aeaac4f457b699b1a878
Viktor Axén <viktor.axen@gmail.com>
2026-04-02 21:18:47 +0200

    treesitter main branch migration

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -1,45 +1,42 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    require("nvim-treesitter").setup({
-      modules = {},
-      sync_install = false,
-      auto_install = true,
-      ignore_install = {},
-      textobjects = {
-        select = {
-          enable = true,
-          keymaps = {
-            ["af"] = { query = "@function.outer", desc = "Around function" },
-            ["if"] = { query = "@function.inner", desc = "Inner function" },
-            ["ac"] = { query = "@class.outer", desc = "Around class" },
-            ["ic"] = { query = "@class.inner", desc = "Inner class" },
-            ["aC"] = { query = "@comment.outer", desc = "Around comment" },
-            ["iC"] = { query = "@comment.inner", desc = "Inner comment" },
-            ["as"] = { query = "@scope.outer", desc = "Around scope" },
-            ["is"] = { query = "@scope.inner", desc = "Inner scope" },
-            ["al"] = { query = "@loop.outer", desc = "Around loop" },
-            ["il"] = { query = "@loop.inner", desc = "Inner loop" },
-            ["ai"] = { query = "@conditional.outer", desc = "Around conditional" },
-            ["ii"] = { query = "@conditional.inner", desc = "Inner conditional" },
-          },
-        },
-        include_surrounding_whitespace = true,
-        lsp_interop = {
-          enable = true,
-          border = require("config").border,
-          floating_preview_opts = {
-            max_height = 22,
-          },
-          peek_definition_code = {
-            ["gpd"] = "@function.outer",
-            ["gpD"] = "@function.inner",
-            ["gpc"] = "@class.outer",
-            ["gpC"] = "@class.inner",
-          },
-        },
+  branch = "main",
+  init = function()
+    vim.g.no_plugin_maps = true
+  end,
+  opts = {
+    select = {
+      lookahead = true,
+      selection_modes = {
+        ["@parameter.outer"] = "v",
+        ["@function.outer"] = "V",
+        ["@class.outer"] = "V",
       },
-    })
+      include_surrounding_whitespace = false,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter-textobjects").setup(opts)
+
+    local select_textobject = require("nvim-treesitter-textobjects.select").select_textobject
+
+    local function map(lhs, query, desc)
+      vim.keymap.set({ "x", "o" }, lhs, function()
+        select_textobject(query, "textobjects")
+      end, { desc = desc })
+    end
+
+    map("af", "@function.outer", "Around function")
+    map("if", "@function.inner", "Inside function")
+    map("ac", "@class.outer", "Around class")
+    map("ic", "@class.inner", "Inside class")
+    map("aC", "@comment.outer", "Around comment")
+    map("iC", "@comment.inner", "Inside comment")
+    map("as", "@scope.outer", "Around scope")
+    map("is", "@scope.inner", "Inside scope")
+    map("al", "@loop.outer", "Around loop")
+    map("il", "@loop.inner", "Inside loop")
+    map("ai", "@conditional.outer", "Around conditional")
+    map("ii", "@conditional.inner", "Inside conditional")
   end,
 }
