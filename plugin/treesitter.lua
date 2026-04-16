afba4aac46faa084ee0c2c27eafd435a3b90d2ce
Viktor Axén <viktor.axen@gmail.com>
2026-04-16 18:54:14 +0200

    autocompile tree-sitter parsers on plugin update

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -4,3 +4,15 @@ vim.pack.add({
     version = "main",
   },
 })
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == "nvim-treesitter" and kind == "update" then
+      if not ev.data.active then
+        vim.cmd.packadd("nvim-treesitter")
+      end
+      vim.cmd("TSUpdate")
+    end
+  end,
+})
